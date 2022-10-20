bb9ce39a00f1cb1d120568d8c1cb889468cdcb00
Kacper <kacper.tylec1999@gmail.com>
2022-10-20 14:28:49 +0200

    preparing validation for creating new bookSwapOffer

--- src/ProtectedPages/CreateSwapOfferPage.tsx
+++ src/ProtectedPages/CreateSwapOfferPage.tsx
@@ -8,6 +8,7 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  Button,
 } from "@mui/material";
 import categories from "../data/categories";
 
@@ -15,19 +16,30 @@ const CreateSwapOfferPage = () => {
   return (
     // name, category, swap for, description, swapPlace, author of the book, image,
     <form>
-      <Container sx={{ height: "100vh" }}>
+      <Container
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
         <Paper
+          elevation={5}
           sx={{
-            height: "80%",
+            minHeight: "80%",
+            width: { xs: "100%", sm: "80%" },
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
-            justifyContent: "center",
-            marginTop: "20px",
           }}
         >
           <Typography variant="h2" margin={"1%"}>
             Your swap offer
+          </Typography>
+
+          <Typography variant="h6">
+            Informations about the book you offer to someone
           </Typography>
 
           {/* input for name of the book -> */}
@@ -37,14 +49,18 @@ const CreateSwapOfferPage = () => {
             id="bookName-textfield"
             label="Enter name of the book"
             variant="outlined"
-            sx={{ width: "80%", marginBottom: "5%" }}
+            sx={{ width: "80%", marginBottom: "2%" }}
           ></TextField>
-          <Stack spacing={2} direction="row" width={"80%"}>
+          <Stack
+            spacing={3}
+            width={"80%"}
+            direction={{ xs: "column", sm: "row" }}
+          >
             {/* select for the category of a book */}
 
-            <FormControl sx={{ width: "33%" }}>
+            <FormControl sx={{ width: { xs: "100%", sm: "33%" } }}>
               <InputLabel variant="standard" htmlFor="categoryOfABook">
-                Category of the book
+                Category
               </InputLabel>
               <Select name="age" id="categoryOfABook">
                 {categories.map<React.ReactNode>((category) => {
@@ -53,21 +69,66 @@ const CreateSwapOfferPage = () => {
               </Select>
             </FormControl>
 
+            {/* input for author */}
+
             <TextField
-              name="bookName"
-              id="bookName-textfield"
-              label="name of the book"
+              name="bookAuthor"
+              id="bookAuthor-textfield"
+              label="Author of the book"
               variant="outlined"
-              sx={{ width: "33%" }}
+              sx={{ width: { xs: "100%", sm: "33%" } }}
             ></TextField>
+
+            {/* input for Book release date (I dont want to install mui x package for one input so I decided to leave it as number input,
+                  so I can anyway validate random inputs with Yup) */}
+
             <TextField
-              name="bookName"
+              type="number"
+              name="bookRelease"
               id="bookName-textfield"
-              label="name of the book"
+              label="Book release date"
               variant="outlined"
-              sx={{ width: "33%" }}
+              sx={{ width: { xs: "100%", sm: "33%" } }}
             ></TextField>
           </Stack>
+
+          <Typography variant="h6" paddingTop={2}>
+            Other informations about the swap offer
+          </Typography>
+
+          <Stack direction="column" spacing={3} width="80%" marginBottom={"5%"}>
+            {/* localization for the swap */}
+            <TextField
+              fullWidth
+              name="swapLocalization"
+              id="swapLocalization-textfield"
+              label="Swap localization"
+              variant="outlined"
+            ></TextField>
+            {/* books somebody want to have */}
+            <TextField
+              fullWidth
+              name="bookAuthor"
+              id="bookExchange-textfield"
+              label="Books that you are interested in for"
+              variant="outlined"
+            ></TextField>
+
+            {/* description */}
+            <TextField
+              name="description"
+              id="description-textfield"
+              label="Description"
+              variant="outlined"
+              multiline
+              minRows={3}
+              maxRows={6}
+            ></TextField>
+          </Stack>
+
+          <Button variant="contained" size="large">
+            Create swap offer
+          </Button>
         </Paper>
       </Container>
     </form>
---
+++ src/data/CreateSwapOfferValidation.ts
@@ -0,0 +1,23 @@
+interface offerInitialValues {
+    nameOfTheBook: string
+    category: string
+    author: string
+    releaseDate: number
+    swapLocalization: string
+    booksInterestedFor: string
+    description: string
+
+}
+
+const initialValues: offerInitialValues = {
+    nameOfTheBook: "",
+    category: "",
+    author: "",
+    releaseDate: 2022,
+    swapLocalization: "",
+    booksInterestedFor: "",
+    description: ""
+}
+
+
+export default initialValues
