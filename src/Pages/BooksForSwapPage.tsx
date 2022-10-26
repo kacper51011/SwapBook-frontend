f2df95f0f9119bc678f9118884147e2ce9df524c
Kacper <kacper.tylec1999@gmail.com>
2022-10-26 17:32:07 +0200

    started work on filterBar

--- src/Pages/BooksForSwapPage.tsx
+++ src/Pages/BooksForSwapPage.tsx
@@ -1,39 +1,36 @@
 import { Container, Stack, Paper, Pagination } from "@mui/material";
 import BookItem from "../components/BookItem";
+import FilterBar from "../components/FilterBar";
 
 const BooksForSwapPage = () => {
   return (
-    <Container
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Paper
-        elevation={1}
+    <>
+      <FilterBar />
+      <Container
         sx={{
-          marginTop: "20px",
-          marginBottom: "20px",
-          paddingTop: "20px",
-          paddingBottom: "20px",
-          backgroundColor: "#F5F5F5",
-          width: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        <Container>
-          <Stack spacing={2}>
-            {/* here will also be searchbar */}
-            <BookItem />
-            <BookItem />
-            <BookItem />
-            <BookItem />
-            <BookItem />
-          </Stack>
-        </Container>
-      </Paper>
-      <Pagination count={10} color="primary" variant="outlined"></Pagination>
-    </Container>
+        <Paper
+          elevation={1}
+          sx={{
+            marginTop: "20px",
+            marginBottom: "20px",
+            paddingTop: "20px",
+            paddingBottom: "20px",
+            backgroundColor: "#F5F5F5",
+            width: 1,
+          }}
+        >
+          <Container>
+            <Stack spacing={2}></Stack>
+          </Container>
+        </Paper>
+        <Pagination count={10} color="primary" variant="outlined"></Pagination>
+      </Container>
+    </>
   );
 };
 
---
+++ src/components/FilterBar.tsx
@@ -0,0 +1,84 @@
+import {
+  Paper,
+  NativeSelect,
+  FormControl,
+  Typography,
+  Stack,
+  Input,
+  Button,
+} from "@mui/material";
+import categories from "../data/categories";
+import SearchBar from "./SearchBar";
+
+const FilterBar = () => {
+  return (
+    <Paper
+      elevation={0}
+      sx={{
+        width: "1",
+        minHeight: "20vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        alignItems: "center",
+        marginTop: "30px",
+        color: "primary.main",
+      }}
+    >
+      {/* searching by name */}
+      <Stack
+        width="80%"
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={5}
+      >
+        <SearchBar />
+      </Stack>
+      {/* category filter */}
+      <Stack
+        width="80%"
+        direction={{ xs: "column", sm: "row" }}
+        justifyContent="space-around"
+        alignItems="center"
+        spacing={5}
+      >
+        <FormControl sx={{ width: { xs: "0.6", sm: "0.15" } }}>
+          <Typography marginBottom="5px">Category</Typography>
+          <NativeSelect defaultValue={""}>
+            <option value=""></option>
+            {categories.map<React.ReactNode>((category, index) => {
+              return (
+                <option key={index} value={category}>
+                  {category}
+                </option>
+              );
+            })}
+          </NativeSelect>
+        </FormControl>
+        {/* books per page  */}
+        <FormControl sx={{ width: { xs: "0.8", sm: "0.15" } }}>
+          <Typography marginBottom="5px">Books per page</Typography>
+
+          <NativeSelect defaultValue={10}>
+            <option value={10}>10</option>
+            <option value={25}>25</option>
+            <option value={50}>50</option>
+          </NativeSelect>
+        </FormControl>
+        {/* sortBy */}
+        <FormControl sx={{ width: { xs: "0.8", sm: "0.15" } }}>
+          <Typography marginBottom="5px">Sort by:</Typography>
+
+          <NativeSelect defaultValue={10}>
+            <option value={10}>10</option>
+            <option value={25}>25</option>
+            <option value={50}>50</option>
+          </NativeSelect>
+        </FormControl>
+      </Stack>
+    </Paper>
+  );
+};
+
+export default FilterBar;
