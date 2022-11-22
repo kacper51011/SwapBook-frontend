f37c10341e7a94587e2eb44e05fbff3ee9035250
Kacper <kacper.tylec1999@gmail.com>
2022-11-22 12:37:26 +0100

    cleaning up async functions and states in Profile/ProfileSecondPaper

--- src/ProtectedPages/AccountSubPages/Profile.tsx
+++ src/ProtectedPages/AccountSubPages/Profile.tsx
@@ -1,10 +1,35 @@
 import { Box, Typography, Grid } from "@mui/material";
 import ProfilePaper from "../../components/ProfilePaper";
 import ProfileSecondPaper from "../../components/ProfileSecondPaper";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setError } from "../../store/alertsSlice";
 
 const Profile = () => {
-  // to do: formik, check validation etc.
+  const dispatch = useDispatch();
+  // states that will store data that useEffect fetch at initial render
+  // also states that will store the default values of inputs in ProfileSecondPaper
+  const [fetchedNickname, setFetchedNickname] = useState<string>("");
+  const [fetchedEmail, setFetchedEmail] = useState<string>("");
+  const [fetchedNumberOfSwaps, setFetchedNumberOfSwaps] = useState<number>();
+  // const [fetchedPhoto, setFetchedPhoto] = useState("")
+
+  useEffect(() => {
+    const getPersonalData = async () => {
+      try {
+        const { data } = await axios.get("/api/users/account/profile");
+        setFetchedEmail(data.user.email);
+        setFetchedNickname(data.user.nickname);
+        setFetchedNumberOfSwaps(data.user.swaps.length);
+      } catch (err) {
+        dispatch(setError("couldn`t load the data, try again later"));
+      }
+    };
+
+    getPersonalData();
+  });
+
   return (
     <Box
       padding={3}
@@ -28,13 +53,18 @@ const Profile = () => {
             xsWidth="1"
             smWidth="1"
             avatarMargin="7vw"
-            email="123"
-            nickname="123"
-            swapsAmount={123}
+            email={fetchedEmail || ""}
+            nickname={fetchedNickname || ""}
+            swapsAmount={fetchedNumberOfSwaps || 0}
           />
         </Grid>
         <Grid item xs={12} sm={8}>
-          <ProfileSecondPaper />
+          <ProfileSecondPaper
+            email={fetchedEmail || ""}
+            nickname={fetchedNickname || ""}
+            setFetchedEmail={setFetchedEmail}
+            setFetchedNickname={setFetchedNickname}
+          />
         </Grid>
       </Grid>
     </Box>
--- src/components/ProfileSecondPaper.tsx
+++ src/components/ProfileSecondPaper.tsx
@@ -9,36 +9,74 @@ import {
 } from "@mui/material";
 import CameraAltIcon from "@mui/icons-material/CameraAlt";
 import EditIcon from "@mui/icons-material/Edit";
-import { MouseEventHandler, useState } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setError, setSuccess } from "../store/alertsSlice";
 
 // component used in account/profile page
 
 interface ISecondPaper {
-  actualNickname?: string;
-  actualPassword?: string;
+  nickname?: string;
+  email?: string;
+  setFetchedEmail: React.Dispatch<React.SetStateAction<string>>;
+  setFetchedNickname: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const ProfileSecondPaper = ({
-  actualNickname,
-  actualPassword,
+  nickname,
+  email,
+  setFetchedEmail,
+  setFetchedNickname,
 }: ISecondPaper) => {
-  const [nicknameEdit, toggleNicknameEdit] = useState<Boolean>(false);
-  const [passwordEdit, togglePasswordEdit] = useState<Boolean>(false);
-  const [emailEdit, toggleEmailEdit] = useState<Boolean>(false);
+  const dispatch = useDispatch();
 
-  const toggleNicknameEditHandler: MouseEventHandler<
-    HTMLButtonElement
-  > = () => {
-    toggleNicknameEdit(!nicknameEdit);
+  // states used only for toggling normal mode and edit mode (Password is a textfield by default, so it dont have a toggle)
+  const [nicknameEdit, toggleNicknameEdit] = useState<Boolean>(false);
+  const [emailEdit, toggleEmailEdit] = useState<Boolean>(false);
+  // states used for storing values of edited fields
+  const [nicknameEditValue, setEditedNicknameValue] = useState("");
+  const [emailEditValue, setEditedEmailValue] = useState("");
+  const [passwordEditValue, setEditedPasswordValue] = useState("");
+
+  const handleNicknameChangeSave = async () => {
+    try {
+      await axios.patch("/api/users/account", {
+        nickname: nicknameEditValue,
+      });
+      dispatch(setSuccess("nickname changed successfully"));
+      setFetchedNickname(nicknameEditValue);
+      toggleNicknameEdit(!nicknameEdit);
+    } catch (err) {
+      dispatch(setError("something went wrong, try other nickname"));
+    }
   };
-  const togglePasswordEditHandler: MouseEventHandler<
-    HTMLButtonElement
-  > = () => {
-    togglePasswordEdit(!passwordEdit);
+
+  const handleEmailChangeSave = async () => {
+    try {
+      await axios.patch("/api/users/account", {
+        email: emailEditValue,
+      });
+      dispatch(setSuccess("nickname changed successfully"));
+      setFetchedEmail(emailEditValue);
+      toggleEmailEdit(!emailEdit);
+    } catch (err) {
+      dispatch(setError("something went wrong, try other nickname"));
+    }
   };
-  const toggleEmailEditHandler: MouseEventHandler<HTMLButtonElement> = () => {
-    toggleEmailEdit(!emailEdit);
+
+  const handlePasswordChangeSave = async () => {
+    try {
+      await axios.patch("/api/users/account", {
+        password: passwordEditValue,
+      });
+      dispatch(setSuccess("password changed successfully"));
+    } catch (err) {
+      dispatch(setError("something went wrong, try other password"));
+    }
   };
+
+  // onChange`s and onClick`s are setted inline
 
   return (
     <Paper
@@ -55,9 +93,9 @@ const ProfileSecondPaper = ({
         Manage your data
       </Typography>
 
-      <Divider orientation="horizontal" />
-      {/* nickname */}
+      <Divider orientation="horizontal" sx={{ marginY: "0.4vw" }} />
 
+      {/* nickname */}
       <Grid
         container
         direction="row"
@@ -70,60 +108,31 @@ const ProfileSecondPaper = ({
           </Typography>
         </Grid>
         <Grid item xs={4}>
-          {!nicknameEdit && <Typography>actualNickname</Typography>}
+          {!nicknameEdit && <Typography>{nickname}</Typography>}
           {nicknameEdit && (
             <TextField
               type="text"
               label="Your Nickname"
               id="nicknameEdit"
               name="nicknameEdit"
+              defaultValue={nickname}
+              onChange={(e) => setEditedNicknameValue(e.target.value)}
             ></TextField>
           )}
         </Grid>
         <Grid item xs={1}>
           {!nicknameEdit && (
-            <IconButton onClick={toggleNicknameEditHandler}>
+            <IconButton onClick={() => toggleNicknameEdit(!nicknameEdit)}>
               <EditIcon />
             </IconButton>
           )}
-          {nicknameEdit && <Button>Save</Button>}
-        </Grid>
-      </Grid>
-      <Divider orientation="horizontal" />
-
-      {/* password */}
-      <Grid
-        container
-        direction="row"
-        alignItems="center"
-        justifyContent="space-around"
-      >
-        <Grid item xs={4} paddingY="1vw">
-          <Typography fontWeight="bold">Your Password</Typography>
-        </Grid>
-        <Grid item xs={4}>
-          {!passwordEdit && <Typography>actualPassword</Typography>}
-          {passwordEdit && (
-            <TextField
-              type="password"
-              label="Your Password"
-              id="passwordEdit"
-              name="passwordEdit"
-            ></TextField>
-          )}
-        </Grid>
-
-        <Grid item xs={1}>
-          {passwordEdit && <Button>Save</Button>}
-          {!passwordEdit && (
-            <IconButton onClick={togglePasswordEditHandler}>
-              <EditIcon />
-            </IconButton>
+          {nicknameEdit && (
+            <Button onClick={handleNicknameChangeSave}>Save</Button>
           )}
         </Grid>
       </Grid>
+      <Divider orientation="horizontal" sx={{ marginY: "0.4vw" }} />
 
-      <Divider orientation="horizontal" />
       {/* email */}
       <Grid
         container
@@ -135,26 +144,55 @@ const ProfileSecondPaper = ({
           <Typography fontWeight="bold">Your Email</Typography>
         </Grid>
         <Grid item xs={4}>
-          {!emailEdit && <Typography>actualEmail</Typography>}
+          {!emailEdit && <Typography>{email}</Typography>}
           {emailEdit && (
             <TextField
               type="email"
               label="Your Email"
               id="emailEdit"
               name="emailEdit"
+              defaultValue={email}
+              onChange={(e) => setEditedEmailValue(e.target.value)}
             ></TextField>
           )}
         </Grid>
         <Grid item xs={1}>
           {!emailEdit && (
-            <IconButton onClick={toggleEmailEditHandler}>
+            <IconButton onClick={() => toggleEmailEdit(!emailEdit)}>
               <EditIcon />
             </IconButton>
           )}
-          {emailEdit && <Button>Save</Button>}
+          {emailEdit && (
+            <Button onClick={() => handleEmailChangeSave}>Save</Button>
+          )}
         </Grid>
       </Grid>
-      <Divider orientation="horizontal" />
+      {/* password */}
+      <Divider orientation="horizontal" sx={{ marginY: "0.4vw" }} />
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justifyContent="space-around"
+      >
+        <Grid item xs={4} paddingY="1vw">
+          <Typography fontWeight="bold">Change password</Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <TextField
+            type="password"
+            label="New Password"
+            id="passwordEdit"
+            name="passwordEdit"
+            onChange={(e) => setEditedPasswordValue(e.target.value)}
+          ></TextField>
+        </Grid>
+
+        <Grid item xs={1}>
+          <Button onClick={() => handlePasswordChangeSave}>Save</Button>
+        </Grid>
+      </Grid>
+      <Divider orientation="horizontal" sx={{ marginY: "0.4vw" }} />
 
       {/* photo  */}
       <Grid
